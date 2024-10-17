a0908b4e47aa3d5f48e07b8daf894919c97b424b
dam2 <dam2@a223e07.dc.domenicoscarlatti.es>
2024-10-17 17:13:35 +0200

    He añadido algo en clase

--- src/desingera/DesingEra.java
+++ src/desingera/DesingEra.java
@@ -6,12 +6,13 @@ package desingera;
 
 /**
  *
- * @author dam2
+ * @author Daniela
  */
 public class DesingEra {
 
     /**
-     * @param args the command line arguments
+     * Es el método principal del proyecto
+     * @param args El array de cadena-palabras que captura el programa
      */
     public static void main(String[] args) {
         // TODO code application logic here
